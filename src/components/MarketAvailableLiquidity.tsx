385116bd999a774450d3b63e3b657b82597b4d1e
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2025-05-07 12:29:29 -0700

    [BLUE-9] Update Key Metrics sections to use customized tooltips (#32)
    * feat(blue-9): Update Metric to export non-tooltip component
    
    * feat(blue-9): Move MarketAvailableLiquidity tooltip content into separate component
    
    * feat(blue-9): Move Apy tooltip content into separate component
    
    * feat(blue-9): Update Borrow page to use customized tooltips for key metrics
    
    * feat(blue-9): Update Earn page to use customized tooltips
    
    * feat(blue-9): Update exports for Metric

--- src/components/MarketAvailableLiquidity.tsx
+++ src/components/MarketAvailableLiquidity.tsx
@@ -1,22 +1,62 @@
 import { formatNumber } from "@/utils/format";
 import { TooltipPopover, TooltipPopoverContent, TooltipPopoverTrigger } from "./ui/tooltipPopover";
 import NumberFlow from "./ui/NumberFlow";
+import { ComponentProps } from "react";
 
-interface MarketAvailableLiquidityProps {
+type MarketAvailableLiquidityProps = {
   liquidityAssetUsd: number;
   publicAllocatorSharedLiquidityAssetsUsd: number;
-}
+  showTooltip?: boolean;
+} & React.ComponentProps<"div">;
+
+type MarketAvailableLiquidityItem = {
+  label: string;
+  value: string;
+};
+
+type MarketAvailableLiquidityTooltipProps = {
+  liquidityAssetUsd: number;
+  publicAllocatorSharedLiquidityAssetsUsd: number;
+};
+
+type MarketAvailableLiquidityTriggerProps = {
+  total: number;
+} & ComponentProps<typeof NumberFlow>;
 
 export default function MarketAvailableLiquidity({
+  className,
   liquidityAssetUsd,
   publicAllocatorSharedLiquidityAssetsUsd,
+  showTooltip = true,
 }: MarketAvailableLiquidityProps) {
   const total = liquidityAssetUsd + publicAllocatorSharedLiquidityAssetsUsd;
 
-  const items: {
-    label: string;
-    value: string;
-  }[] = [
+  return showTooltip ? (
+    <TooltipPopover>
+      <TooltipPopoverTrigger>
+        <MarketAvailableLiquidityTrigger className={className} total={total} />
+      </TooltipPopoverTrigger>
+      <TooltipPopoverContent className="w-[320px]">
+        <MarketAvailableLiquidityTooltip
+          liquidityAssetUsd={liquidityAssetUsd}
+          publicAllocatorSharedLiquidityAssetsUsd={publicAllocatorSharedLiquidityAssetsUsd}
+        />
+      </TooltipPopoverContent>
+    </TooltipPopover>
+  ) : (
+    <MarketAvailableLiquidityTrigger className={className} total={total} />
+  );
+}
+
+function MarketAvailableLiquidityTrigger({ total, ...props }: MarketAvailableLiquidityTriggerProps) {
+  return <NumberFlow value={total} format={{ currency: "USD" }} {...props} />;
+}
+
+export function MarketAvailableLiquidityTooltip({
+  liquidityAssetUsd,
+  publicAllocatorSharedLiquidityAssetsUsd,
+}: MarketAvailableLiquidityTooltipProps) {
+  const items: MarketAvailableLiquidityItem[] = [
     {
       label: "Liquidity in Market",
       value: formatNumber(liquidityAssetUsd, { currency: "USD" }),
@@ -27,31 +67,28 @@ export default function MarketAvailableLiquidity({
     },
   ];
 
+  const total = liquidityAssetUsd + publicAllocatorSharedLiquidityAssetsUsd;
+
   return (
-    <TooltipPopover>
-      <TooltipPopoverTrigger>
-        <NumberFlow value={total} format={{ currency: "USD" }} />
-      </TooltipPopoverTrigger>
-      <TooltipPopoverContent className="flex w-[320px] flex-col gap-4">
-        <span className="label-md">Market Liquidity</span>
-        <p className="text-content-secondary paragraph-sm">
-          The total amount of assets available for borrowing, including liquidity that can be reallocated from other
-          markets through the public allocator.
-        </p>
-        <div className="flex flex-col gap-2 paragraph-sm">
-          {items.map((item, i) => (
-            <div className="flex items-center justify-between" key={i}>
-              <span>{item.label}</span>
-              <span className="label-sm">{item.value}</span>
-            </div>
-          ))}
-        </div>
-        <div className="h-[1px] w-full bg-border-primary" />
-        <div className="flex items-center justify-between label-md">
-          <span>Total Available Liquidity</span>
-          <span>{formatNumber(total, { currency: "USD" })}</span>
-        </div>
-      </TooltipPopoverContent>
-    </TooltipPopover>
+    <div className="flex flex-col gap-4">
+      <span className="label-md">Market Liquidity</span>
+      <p className="text-content-secondary paragraph-sm">
+        The total amount of assets available for borrowing, including liquidity that can be reallocated from other
+        markets through the public allocator.
+      </p>
+      <div className="flex flex-col gap-2 paragraph-sm">
+        {items.map((item, i) => (
+          <div className="flex items-center justify-between" key={i}>
+            <span>{item.label}</span>
+            <span className="label-sm">{item.value}</span>
+          </div>
+        ))}
+      </div>
+      <div className="h-[1px] w-full bg-border-primary" />
+      <div className="flex items-center justify-between label-md">
+        <span>Total Available Liquidity</span>
+        <span>{formatNumber(total, { currency: "USD" })}</span>
+      </div>
+    </div>
   );
 }
